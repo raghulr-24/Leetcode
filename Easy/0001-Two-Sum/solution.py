a04e2b2ff8e8a33a6dd0c851efd8f58fff4ee65d
Raghul Rajendran <raghulrajendran24@gmail.com>
2026-07-30 21:11:52 +0100

    🟢 Add 0001. Two Sum [Easy] (Python3)

--- Easy/0001-Two-Sum/solution.py
+++ Easy/0001-Two-Sum/solution.py
@@ -1,18 +1,19 @@
 # ═══════════════════════════════════════════════════════
 #  Problem  : 0001. Two Sum
-#  URL      : https://leetcode.com/problems/two-sum/submissions/2086712605/
+#  URL      : https://leetcode.com/problems/two-sum/
 #  Difficulty : Easy
 #  Language : Python3
 #  Runtime  : 0 ms
-#  Memory   : 20.5 MB
-#  Solved   : July 29, 2026
+#  Memory   : 19.4 MB
+#  Solved   : July 30, 2026
 # ═══════════════════════════════════════════════════════
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         table = {}
         for i, n in enumerate(nums):
-            res = target - n
-            if res in table:
-                return [table[res], i]
-            table[n] = i
+            diff = target - n
+            if diff in table:
+                return [table[i], n]
+        table[i] = n
+        
